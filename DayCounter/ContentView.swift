8025ae17811ef30f6e9c2bc688b2d3fe95c006db
hyunjun <hyunjuntyler@gmail.com>
2023-08-23 02:04:47 +0900

    add: ContentView 추가

--- DayCounter/ContentView.swift
+++ DayCounter/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("onboarding") var onboarding: Bool = true
+    
+    init() {
+        var titleFont = UIFont.preferredFont(forTextStyle: .largeTitle) // the default large title font
+        titleFont = UIFont(
+            descriptor:
+                titleFont.fontDescriptor
+                .withDesign(.rounded)? // make rounded
+                .withSymbolicTraits(.traitBold) // make bold
+            ??
+            titleFont.fontDescriptor, // return the normal title if customization failed
+            size: titleFont.pointSize
+        )
+        var inlineFont = UIFont.preferredFont(forTextStyle: .body) // the default large body
+        inlineFont = UIFont(
+            descriptor:
+                inlineFont.fontDescriptor
+                .withDesign(.rounded)? // make rounded
+                .withSymbolicTraits(.traitBold) // make bold
+            ??
+            inlineFont.fontDescriptor, // return the normal title if customization failed
+            size: inlineFont.pointSize
+        )
+        
+        // set the rounded font
+        UINavigationBar.appearance().largeTitleTextAttributes = [.font: titleFont]
+        UINavigationBar.appearance().titleTextAttributes = [.font: inlineFont]
+        
+        UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).tintColor = .systemMint
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        MainView()
+            .sheet(isPresented: $onboarding) {
+                OnboardingView()
+                    .interactiveDismissDisabled()
+            }
     }
 }
 
