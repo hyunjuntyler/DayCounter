03a4300115acfa3fdaa6427cab5df9ea3961cad7
hyunjun <hyunjuntyler@gmail.com>
2023-08-23 01:02:17 +0900

    init: 프로젝트 시작

---
+++ DayCounter/DayCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayCounterApp.swift
+//  DayCounter
+//
+//  Created by hyunjun on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DayCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
